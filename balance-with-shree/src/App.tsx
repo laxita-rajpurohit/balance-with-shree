baa503f680d8274bb601db51370a0679459fda11
laxita-rajpurohit <laxitarajpurohit776@gmail.com>
2025-12-18 02:59:01 +0530

    created background leaf animation

--- balance-with-shree/src/App.tsx
+++ balance-with-shree/src/App.tsx
@@ -1,14 +1,41 @@
+import { useEffect, useRef } from "react";
 import { Navbar } from "./components/Navbar";
 import AppRoutes from "./Routes/AppRoutes";
 
 const App = () => {
+  const leafRightRef = useRef<HTMLDivElement>(null);
+  const leafLeftRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (window.innerWidth < 768) return;
+
+    const onScroll = () => {
+      const offset = window.scrollY * 0.25;
+
+      if (leafRightRef.current) {
+        leafRightRef.current.style.transform = `translateY(${offset}px)`;
+      }
+
+      if (leafLeftRef.current) {
+        leafLeftRef.current.style.transform = `translateY(${offset}px)`;
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
   return (
-    <>
-      <Navbar />
-      <AppRoutes />
-    </>
+    <div className="site-wrapper">
+      <div ref={leafRightRef} className="leaf-background leaf-right" />
+      <div ref={leafLeftRef} className="leaf-background leaf-left" />
+
+      <div className="content">
+        <Navbar />
+        <AppRoutes />
+      </div>
+    </div>
   );
 };
 
 export default App;
-
--- balance-with-shree/src/styles/GlobalStyles.ts
+++ balance-with-shree/src/styles/GlobalStyles.ts
@@ -1,4 +1,6 @@
 import { createGlobalStyle } from "styled-components";
+import leafRight from "../assets/leaf.svg";
+import leafLeft from "../assets/right leaf.svg";
 
 export const GlobalStyles = createGlobalStyle`
   * {
@@ -17,4 +19,43 @@ export const GlobalStyles = createGlobalStyle`
     font-family: 'Inter', sans-serif;
    color: ${({ theme }) => theme.colors.muted};
   }
+   .page {
+   position: relative;
+ }
+
+.site-wrapper {
+ min-height: 100vh;
+
+}
+ /* 🌿 Base leaf styles */
+.leaf-background {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 100vh;
+
+  background-repeat: no-repeat;
+  // background-size: 520px auto;
+
+  opacity: 0.40;
+  pointer-events: none;
+  z-index: 0;
+  will-change: transform;
+}
+
+/* 🌿 Right leaf */
+.leaf-right {
+  left: 0;
+  background-image: url(${leafRight});
+  background-position: right -10px center;
+}
+
+/* 🌿 Left leaf */
+.leaf-left {
+  left: 0;
+  background-image: url(${leafLeft});
+  background-position: left -10px center;
+}
+
+
 `;
